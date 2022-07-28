5633fc71cc653d9dfd8668021c90cd6be466b527
Alfredo Blanco <alfre.blanco@icloud.com>
2022-07-28 19:05:00 -0300

    Finished

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
 const server = require('./src/app.js');
+const dotenv = require('dotenv').config();
 
-server.listen(process.env.PORT, () => console.log('Server listening'));
+server.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}`));
