48f9731081b46916e57d0daf5f354fcb131c2acb
Alfredo Blanco <alfre.blanco@icloud.com>
2023-02-22 19:09:57 -0300

    Try

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: ["*"],
         methods: [ "GET", "POST"],
         credentials: true,
     }
